b86488712756e48103306be6fbf8494970bb0aa3
Zimo Xiao <xiaozimo@zuggr.com>
2021-07-28 11:31:44 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import json
+import os
 
-print('running main...')
+print('running main...\n')
 
+print('./job.json\n')
 job = json.load(open('./job.json',))
+
+print('SLURM_NODEID\n')
+print(os.environ['SLURM_NODEID'])
 
 print(job)
